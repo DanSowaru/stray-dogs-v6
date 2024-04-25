c76e31bf181d455387f12dde085dfded144a8fd7
Danilo Soares <prof.danilosoares@gmail.com>
2024-04-25 16:30:01 -0300

    home modal first draft

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="home">
-    <h1>Welcome to Stray Dogs!</h1>
+  <div id="home-screen">
+
+    <div id="home-container">
+
+      <img id="splash_screen" src="@/assets/images/splash_screen.png" alt="stray dogs splash screen">
+
+      <h1>Welcome to Stray Dogs!</h1>
+
+      <div id="login-form-container">
+        <button id="google-login">Google Placeholder</button>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
